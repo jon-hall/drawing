7b8f8af9be7b3606a42de12a67cea074a167115b
Jon Hall <jon_hall@outlook.com>
2016-03-20 23:16:10 +0000

    update drawify

--- client/drawify.js
+++ client/drawify.js
@@ -1,18 +1,20 @@
 // Bookmarklet helper to create a canvas to draw on within the next clicked element
 // - assumes Drawing is already defined
-function drawify(event) {
-    window.removeEventListener('click', drawify);
+function drawify(socket, our_id) {
+    function make_drawing(event) {
+        window.removeEventListener('click', make_drawing);
 
-    var canvas = document.createElement('canvas');
+        var canvas = document.createElement('canvas');
 
-    canvas.width = window.DRAWING_WIDTH || 600;
-    canvas.height = window.DRAWING_HEIGHT || 400;
-    canvas.style.background = 'white';
-    canvas.style.cursor = 'pointer';
+        canvas.width = window.DRAWING_WIDTH || 600;
+        canvas.height = window.DRAWING_HEIGHT || 400;
+        canvas.style.background = 'white';
+        canvas.style.cursor = 'pointer';
 
-    event.target.appendChild(canvas);
+        event.target.appendChild(canvas);
 
-    // This goes nowhere...
-    return new Drawing(canvas);
+        // This goes nowhere...
+        return new Drawing(canvas, socket, our_id);
+    }
+    window.addEventListener('click', make_drawing);
 }
-window.addEventListener('click', drawify);
