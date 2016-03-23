34383bbb4d6369e6df9d3734c49699ea030c59aa
Jon Hall <jon_hall@outlook.com>
2016-03-23 22:32:36 +0000

    sharing sockets

--- index.js
+++ index.js
@@ -1,4 +1,7 @@
 exports.io = function() {
+    var sockets = {},
+        existing;
+
     return function(socket, next) {
         var draw_event = 'draw:' + socket.id,
             move_event = 'move:' + socket.id;
@@ -13,11 +16,23 @@ exports.io = function() {
 
         socket.on('disconnect', function() {
             socket.to('drawing').emit('leave', socket.id);
+            delete sockets[socket.id];
+        });
+
+        socket.on('ready_to_draw', function() {
+            // Tell the client about everyone else, when they're ready
+            for(existing in sockets) {
+                if(sockets.hasOwnProperty(existing)) {
+                    socket.emit('join', existing);
+                }
+            }
+            sockets[socket.id] = socket;
         });
 
         socket.join('drawing');
         socket.to('drawing').emit('join', socket.id);
-
+        socket.emit('ready', socket.id);
+        
         return next();
     };
 };
